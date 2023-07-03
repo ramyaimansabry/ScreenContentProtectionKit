9fbddb6f1e5f44c5802ea7fed02ecc9252c172ad
Ramy Sabry <ramy.sabry@fawry.com>
2023-07-03 23:25:16 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ScreenContentProtectionKit",
-            path: "ScreenContentProtectionKit/Sources")
+            path: "./ScreenContentProtectionKit/Source")
     ]
 )
