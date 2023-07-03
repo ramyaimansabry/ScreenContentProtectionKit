00f84644c16c828050c494e76416f37e2b4c6917
Ramy Sabry <ramy.sabry@fawry.com>
2023-07-03 23:02:06 +0300

    Initial Commit

---
+++ ScreenContentProtectionKit/ScreenContentProtectionKit.h
@@ -0,0 +1,18 @@
+//
+//  ScreenContentProtectionKit.h
+//  ScreenContentProtectionKit
+//
+//  Created by Ramy Sabry on 03/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScreenContentProtectionKit.
+FOUNDATION_EXPORT double ScreenContentProtectionKitVersionNumber;
+
+//! Project version string for ScreenContentProtectionKit.
+FOUNDATION_EXPORT const unsigned char ScreenContentProtectionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenContentProtectionKit/PublicHeader.h>
+
+
